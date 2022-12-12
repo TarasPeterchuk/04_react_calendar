c667bc4187a4c15bd3542837ab54e643d0fa6f08
apache0505 <jiayulutsk@ukr.net>
2022-12-12 10:49:55 +0200

    csd

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = (env, argv) => {
@@ -36,6 +37,15 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: './src/index.html',
       }),
+      new CopyPlugin({
+        patterns: [
+          { from: '_redirects', to: '' },
+          {
+            from: 'source/url/to/images/folder',
+            to: 'dest/url/to/images/folder',
+          },
+        ],
+      }),
     ],
     devServer: {
       historyApiFallback: true,
@@ -53,7 +63,7 @@ module.exports = (env, argv) => {
     config.plugins.push(
       new MiniCssExtractPlugin({
         filename: '[name].css',
-      }),
+      })
     );
   }
 
